c5da8be617d03d6f969444516db9820cc7801934
Vraj Patel <vrajpatel06021998@gmail.com>
2026-01-25 21:58:15 +0530

    fix : proxy.ts file added and change protected route

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
   "/",
-  "/api/webhooks/clerk"
-])
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks/(.*)", // 👈 MUST be wildcard
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
